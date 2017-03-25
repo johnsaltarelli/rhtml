4a2cf245d2413dd82a75ef1f06e5ae633b9998f5
john@digitallyft.com <john@digitallyft.com>
2017-03-25 19:39:56 -0400

    updated specs to include new text method

--- spec/rhtml_spec.rb
+++ spec/rhtml_spec.rb
@@ -8,10 +8,17 @@ describe Rhtml do
 
   describe Rhtml::Block do
 
+    it 'adds tag methods to instance' do
+      instance = Rhtml::Block.new
+      Rhtml::Block::TAGS.each do |tag|
+        expect(instance.respond_to?(tag)).to eq(true)
+      end
+    end
+
     it 'generates un-closed HTML tags' do
       Rhtml::Block::TAGS.each do |tag|
         block = Rhtml::Block.new do
-          send(tag, { class: 'my-class', id: 'my-id-1234'})
+          send(tag, { class: 'my-class', id: 'my-id-1234' })
         end
         result = "<#{tag} class=\"my-class\" id=\"my-id-1234\">"
         expect(block.content).to eq(result)
@@ -34,11 +41,18 @@ describe Rhtml do
           li 'This is a test', class: 'my-class', id: 'my-id'
           li 'this is another test', class: 'your-class', id: 'another-id'
         end
+      end
+      result = "<ul><li class=\"my-class\" id=\"my-id\">This is a test</li><li class=\"your-class\" id=\"another-id\">this is another test</li></ul>"
+      expect(output.content).to eq(result)
+    end
+
+    it 'generates nested HTML tags with text' do
+      output = Rhtml::Block.new do
         p class: 'my-paragraph' do
-          span 'my inner span text'
+          text 'my inner text'
         end
       end
-      result = "<ul><li class=\"my-class\" id=\"my-id\">This is a test</li><li class=\"your-class\" id=\"another-id\">this is another test</li></ul><p class=\"my-paragraph\"><span>my inner span text</span></p>"
+      result = "<p class=\"my-paragraph\">my inner text</p>"
       expect(output.content).to eq(result)
     end
 
